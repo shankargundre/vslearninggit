ebce5f3ad4c636b45719f95f66e9c1c1155d638c
shankarg <shankargundre@gmail.com>
2025-04-21 07:22:15 +0530

    test

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ axios.get('https://jsonplaceholder.typicode.com/todos/1')
 
   import axios from "axios";
 axios.get('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => console.log(response.data))
+  .then(response => console.log(response.data))git 
